9a012de7f0ba314272a1231bbe317d66782f081e
David van Capelle <david.van.capelle@gmail.com>
2021-02-01 21:41:40 +0100

    first commit

--- script.js
+++ script.js
@@ -1,10 +1,53 @@
-const huiswerkMaken = (vak, callback) => {
-    setTimeout( () => console.log(`Ok, ok ik ga nu mijn ${vak} huiswerk maken`), 2500);
-    callback();
+const testNum = (num) => {
+    return new Promise((success, reject) => {
+        if (num < 10) {
+            success("Yay! Success!");
+        } else {
+            reject("Bummer! No Luck this time!");
+        }
+    })
+        .then(res => console.log(res))
+        .catch(err => console.log(err));
+}
+testNum(9);
+testNum(15);
+
+const arrayOfWords = ['cucumber', 'tomatos', 'avocado'];
+const complicatedArray = ['cucumber', 44, true];
+
+
+const makeAllCaps = (words) => {
+    return new Promise((resolve, reject) => {
+        if (words.every(word => typeof word === "string")) {
+            resolve(sortWords(words.map(word => word.toUpperCase())))
+
+        } else {
+            reject('error error error not all strings!')
+        }
+    })
+    //.then(res => console.log(res))
+    //.catch(err => console.log(err));
 }
 
-const klaarMetHuiswerk = () => {
-    console.log(`Kijk Paps/Mams, ik ben klaar met mijn huiswerk!`)
+const sortWords = (words) => {
+    return new Promise((resolve, reject) => {
+        if (words) {
+            resolve(words.sort());
+        } else {
+            reject('hahahaha no go')
+        }
+    })
 }
 
-huiswerkMaken('wiskunde', klaarMetHuiswerk)
+makeAllCaps(arrayOfWords)
+    //.then(sortWords(arrayOfWords))
+    .then(res => console.log(res))
+    .catch(err => console.log(err));
+
+makeAllCaps(complicatedArray)
+    //.then(sortWords(complicatedArray))
+    .then(res => console.log(res))
+    .catch(err => console.log(err));
+
+
+console.log('test')
